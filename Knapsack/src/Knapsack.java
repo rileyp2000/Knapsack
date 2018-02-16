2a3ffd9bb8c594a70247f0a074f2b8c9118a21bd
rileyp2000 <rileyp@bishopireton.org>
2018-02-16 15:02:39 -0500

    a  working, b not

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -12,7 +12,7 @@ public class Knapsack {
 		if(n<0 || w[n] > limit)
 			return 0;
 		else {
-			
+						
 			int excLast = knapsackSumA(w,n-1,limit);
 			int incLast = w[n] + knapsackSumA(w,n-1,limit - w[n]);
 			
@@ -23,6 +23,45 @@ public class Knapsack {
 		}
 	}
 	
+	
+	public static int knapsackSumB(int[] w, int n, int limit, List<Integer> list) {
+		if(n<0 || w[n] > limit)
+			return 0;
+		else {
+			
+			//Lists to add
+			List<Integer> list1 = new ArrayList<Integer>();
+			list1.addAll(list);
+			List<Integer> list2 = new ArrayList<Integer>();
+			list2.addAll(list);
+			list2.add(w[n]);
+			
+			int excLast = knapsackSumA(w,n-1,limit);
+			
+			int incLast = w[n] + knapsackSumA(w,n-1,limit - w[n]);
+			
+			if(Math.max(incLast, excLast) == incLast) {
+			
+				list2.add(w[n]);
+				list = list
+				return incLast;
+			
+			}else
+				
+				return excLast;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 	public static int knapsackSumB(int[] w, int n, int limit,List<Integer> list) {
 		//int weight = w[n];
 		if(n<0 || w[n] > limit)
@@ -55,7 +94,7 @@ public class Knapsack {
 				return excLast;
 		}
 	}
-	
+	*/
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>();
 		
